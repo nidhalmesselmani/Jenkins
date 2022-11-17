e2eaf6316e6c3ac04594da1224e1392d835ee2f7
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-17 11:30:31 +0100

    added a logger

--- my_cpp_app/source/main.cpp
+++ my_cpp_app/source/main.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 #include "dog.h"
 #include "operations.h"
+#include "log.h"
 
 int main(){
-    Dog dog1("Rex");
-    dog1.print_info();
+    double result = add(2,70);
+    std::cout << "result : " << result << std::endl;
+
+    Dog dog("Rex");
+    dog.print_info();
+
+    log_data("Hello there",LogType::FATAL_ERROR);
+
     return 0;
 }
