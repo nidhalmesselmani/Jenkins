a4217e985fd6c93d2b699693c7d4a44e4b8749ae
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-17 14:27:11 +0100

    used cpp20

--- my_cpp_app/source/main.cpp
+++ my_cpp_app/source/main.cpp
@@ -5,12 +5,19 @@
 
 int main(){
     double result = add(2,70);
+    auto testCpp20 = (10 <=> 20) > 0;
+    std::cout << testCpp20 << "\n";
+
+    if (testCpp20)
+        std::cout << "10 > 20\n";
+    else
+        std::cout << "10 < 20\n";
     std::cout << "result : " << result << std::endl;
 
     Dog dog("Rex");
     dog.print_info();
 
-    log_data("Hello there ",LogType::MESSAGE);
+    log_data("Hello there  How are you doing ",LogType::MESSAGE);
 
     return 0;
 }
