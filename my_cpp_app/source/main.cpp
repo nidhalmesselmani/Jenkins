494adfc7df22e846e7f8f5f71f6423698cdc681b
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-17 18:16:52 +0100

    test

--- my_cpp_app/source/main.cpp
+++ my_cpp_app/source/main.cpp
@@ -4,15 +4,7 @@
 #include "log.h"
 
 int main(){
-    double result = add(2,70);
-    auto testCpp20 = (10 <=> 20) > 0;
-    std::cout << testCpp20 << "\n";
 
-    if (testCpp20)
-        std::cout << "10 > 20\n";
-    else
-        std::cout << "10 < 20\n";
-    std::cout << "result : " << result << std::endl;
 
     Dog dog("Rex");
     dog.print_info();
