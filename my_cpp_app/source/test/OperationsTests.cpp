beccbff3f56671df3677c3d8be15877e78cbbd30
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-17 17:41:01 +0100

    added unit test support

---
+++ my_cpp_app/source/test/OperationsTests.cpp
@@ -0,0 +1,28 @@
+#include<gtest/gtest.h>
+
+int MAC(int x, int y, int& sum){
+    sum+= x * y - 1;
+    return sum; 
+}
+
+int side_effect = 42;
+bool f(){
+    side_effect =16;
+    return false;
+}
+
+TEST(OperationsTests, MAC){
+    int x = 42;
+    int y = 16;
+    int sum = 100;
+    int oldSum = sum;
+    int expectedNewSum = oldSum + x * y;
+    EXPECT_EQ(
+        oldSum + x * y,
+        MAC(x,y,sum)
+    );
+    EXPECT_EQ(
+        expectedNewSum,
+        sum
+    );
+}
