0d5d9d0e9a953ac244afdd88f9da67c275046001
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-18 01:16:04 +0100

    EXPECT_STREQ ASSERT_STREQ

--- my_cpp_app/source/test/OperationsTests.cpp
+++ my_cpp_app/source/test/OperationsTests.cpp
@@ -1,8 +1,37 @@
 #include<iostream>
 #include<gtest/gtest.h>
 
-TEST(TestName, Subtest_1)
+class MyClass {
+    std::string id;
+public:
+    MyClass(std::string _id) : id(_id){}
+    std::string GetId() { return id; }
+};
+
+TEST(TestName, increment_by_5)
 {
-    EXPECT_EQ(2,2);
-    std::cout << "After assertion" << std::endl;
+    // Arrange
+    MyClass mc("root");
+
+
+    // Act
+    std::string value = mc.GetId();
+
+    // Assert
+    EXPECT_STREQ(value.c_str(),"root");
+
+}
+
+TEST(TestName, increment_by_10)
+{
+    // Arrange
+    int value = 100;
+    int increment = 10;
+
+    // Act
+    value = value + increment;
+
+    // Assert
+    ASSERT_EQ(value,110);
+
 }
