4e6b7f009f508a5af78bd36fbb0dca1edca5861a
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-18 02:08:17 +0100

    Test Fixtures

--- my_cpp_app/source/test/OperationsTests.cpp
+++ my_cpp_app/source/test/OperationsTests.cpp
@@ -1,37 +1,82 @@
 #include<iostream>
 #include<gtest/gtest.h>
+#include<vector>
 
-class MyClass {
-    std::string id;
+class Stack {
+    std::vector<int> vstack = {};
 public:
-    MyClass(std::string _id) : id(_id){}
-    std::string GetId() { return id; }
+    void push(int value) { vstack.push_back(value); }
+    int pop() { 
+        if (vstack.size() > 0 ) {
+            int value = vstack.back();
+            vstack.pop_back();
+            return value;
+        }else{
+            return -1;
+        }
+    }
+    int size() {  return vstack.size();  }
 };
 
-TEST(TestName, increment_by_5)
-{
-    // Arrange
-    MyClass mc("root");
+class MyClass {
+    int baseValue;
+public:
+    MyClass(int _bv) : baseValue(_bv){}
+    void Increment(int byValue){
+        baseValue += byValue;
+    }
+    int GetValue() { return baseValue; }
+};
 
+struct stackTest : public testing::Test {
+    Stack s1;
+    void SetUp() { 
+        std::cout <<"Alive Stack"<< std::endl;
+        int value[] = {1,2,3,4,5,6,7,8,9};
+        for (auto &val : value){
+            s1.push(val);
+        }
+                 
+    }
+    void TearDown() { std::cout <<"DEAD stack"<< std::endl;}
+};
 
-    // Act
-    std::string value = mc.GetId();
-
-    // Assert
-    EXPECT_STREQ(value.c_str(),"root");
-
+TEST_F(stackTest, PopTest){
+    // ACT
+    int lastPoppedValue = 9;
+    // ASSERT
+    while(lastPoppedValue != 1)
+        ASSERT_EQ(s1.pop(), lastPoppedValue--);
+}
+TEST_F(stackTest, sizeValidityTest){
+    // ACT
+    int val = s1.size();
+    // ASSERT
+    for(val; val > 0; val--)
+        ASSERT_NE(s1.pop(), -1);
 }
 
-TEST(TestName, increment_by_10)
-{
-    // Arrange
-    int value = 100;
-    int increment = 10;
+
+struct MyClassTest : public testing::Test {
+    MyClass *mc;
+    void SetUp() { std::cout <<"Alive"<< std::endl;  mc = new MyClass(100) ;}
+    void TearDown() { std::cout <<"DEAD"<< std::endl; delete mc;}
+};
+
+TEST_F(MyClassTest, increment_by_5){
 
     // Act
-    value = value + increment;
+    mc->Increment(5);
 
-    // Assert
-    ASSERT_EQ(value,110);
+    // ASSERT
+    ASSERT_EQ(mc->GetValue(), 105);
+}
 
+TEST_F(MyClassTest, increment_by_10){
+
+    // Act
+    mc->Increment(10);
+
+    // ASSERT
+    ASSERT_EQ(mc->GetValue(), 110);
 }
