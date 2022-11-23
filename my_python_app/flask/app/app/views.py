45092fa33be6929be2e02c1bf6d5c4984f7b5efd
nidhal <nidhalmesselmani@yahoo.fr>
2022-11-24 00:03:04 +0100

    Serving HTML files

--- my_python_app/flask/app/app/views.py
+++ my_python_app/flask/app/app/views.py
@@ -1,8 +1,10 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/")
 def index():
-    return "hi"
+    return render_template("public/index.html")
 
 @app.route("/about")
 def about():
